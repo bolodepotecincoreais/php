27e4c9f8b83ab260dcce13f386a0be72c69f4302
bolodepotecincoreais <160507873+bolodepotecincoreais@users.noreply.github.com>
2024-03-13 12:06:14 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Teste do xampp</title>
+</head>
+<body>
+    <h1>Pagina test html</h1>
+
+    <?php
+            echo "Olá, eu sou um script PHP!";
+        ?>
+
+</body>
+</html>
